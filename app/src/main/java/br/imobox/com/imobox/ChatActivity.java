8e97f783a8f929fee3f33843ffa7df94fbb3382a
Wilder Pereira <wilder_roberto@hotmail.com>
2017-11-15 11:39:59 -0200

    add corretor rating

--- app/src/main/java/br/imobox/com/imobox/ChatActivity.java
+++ app/src/main/java/br/imobox/com/imobox/ChatActivity.java
@@ -1,18 +1,32 @@
 package br.imobox.com.imobox;
 
+import android.content.DialogInterface;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import br.imobox.com.imobox.adapter.ChatAdapter;
+import br.imobox.com.imobox.helper.DownloadImageTask;
 import br.imobox.com.imobox.model.Message;
+import br.imobox.com.imobox.utils.Constants;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -48,5 +62,52 @@ public class ChatActivity extends AppCompatActivity {
                 }
             }, 2000);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chat_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.rate){
+            displayDialog();
+        } else {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayDialog(){
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View deleteDialogView = factory.inflate(R.layout.rate_dialog, null);
+        final AlertDialog deleteDialog = new AlertDialog.Builder(this).create();
+        deleteDialog.setView(deleteDialogView);
+        final RatingBar starsBar = deleteDialogView.findViewById(R.id.rating_stars_gn);
+        starsBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(ChatActivity.this, v+" estrelas dadas ao corretor.", Toast.LENGTH_SHORT).show();
+                deleteDialog.dismiss();
+            }
+        });
+//                starsBar.setOnTouchListener(new View.OnTouchListener() {
+//                    @Override
+//                    public boolean onTouch(View view, MotionEvent motionEvent) {
+//                        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+//                            float touchPositionX = motionEvent.getX();
+//                            float width = starsBar.getWidth();
+//                            float starsf = touchPositionX / width * 5.0f;
+//                            float stars = starsf + 1;
+//                            starsBar.setRating(stars);
+//                            deleteDialog.dismiss();
+//                        }
+//                        return true;
+//                    }
+//                });
+        deleteDialog.show();
     }
 }
