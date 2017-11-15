aad4648a6d8fcac76e779eb4f2e5186daee52de4
mcatossi <mcatossi@br.ibm.com>
2017-11-15 01:14:23 -0200

    create screen init

--- app/src/main/java/br/imobox/com/imobox/MainActivity.java
+++ app/src/main/java/br/imobox/com/imobox/MainActivity.java
@@ -2,12 +2,19 @@ package br.imobox.com.imobox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_client, btn_realtor, btn_owner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_client  = findViewById(R.id.btn_client);
+        btn_realtor = findViewById(R.id.btn_realtor);
+        btn_owner   = findViewById(R.id.btn_owner);
+
+
     }
 }
