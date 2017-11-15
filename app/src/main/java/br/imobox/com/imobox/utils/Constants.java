ff80dc760d5917a5b6164c999b7e43ab2fd531f7
Wilder Pereira <wilder_roberto@hotmail.com>
2017-11-15 08:28:30 -0200

    improve info property dialog

--- app/src/main/java/br/imobox/com/imobox/utils/Constants.java
+++ app/src/main/java/br/imobox/com/imobox/utils/Constants.java
@@ -1,7 +1,5 @@
 package br.imobox.com.imobox.utils;
 
-import java.util.ArrayList;
-
 /**
  * Created by matheuscatossi on 15/11/17.
  */
@@ -11,7 +9,7 @@ public class Constants {
     final public static String[] title = {"Apartamento com 3 Quartos para Alugar, 92 m² por R$ 2.800/Mês", "Apartamento com 3 Quartos para Alugar, 120 m² por R$ 2.300/Mês", "Apartamento com 3 Quartos para Venda ou Aluguel, 120 m² por R$ 1.420.000", "Apartamento com 3 Quartos para Alugar, 140 m² por R$ 3.000/Mês"};
     final public static String[] type = {"ALUGUEL", "ALUGUEL", "ALUGUEL", "ALUGUEL"};
     final public static String[] price = {"R$ 2.800 / Mês", "R$ 2.300 / Mês", "R$ 5.000 / Mês", "R$ 3.000 / Mês"};
-    final public static String[] additional = {"CONDOMÍNIO: R$ 750", "CONDOMÍNIO: R$ 912", "CONDOMÍNIO: R$ 1.190", "CONDOMÍNIO: R$ 1.165"};
+    final public static String[] additional = {"R$ 750", "R$ 912", "R$ 1.190", "R$ 1.165"};
     final public static String[] typeImo = {"Apartamento", "Apartamento", "Apartamento", "Apartamento"};
     final public static String[] info = {"Apto primeira locação, 92 m², 3 dormitórios (1 suíte) todos com armários, 2 banheiros com todos os acessórios, living 2 ambientes, fantástico terraço gourmet com churrasqueira, pia com gabinete, confortável cozinha com armários, área de serviço com armários e aquecedor a gás, 2 vagas de garagem cobertas. Edifício conta com completa área de lazer e convívio social, inclusive sala de cinema, quadra de squash, verde e lazer se completam.", "Pintura nova, sala com piso tábua corrida, dormitórios com piso taco madeira ( novo ), dormitórios com armários, cozinha com armários, banheiro com box Próximo do centro de Santana e do metrô. Condomínio com grande área comum e lazer completo.", "Belíssimo apartamento, face norte, vista para Serra da Cantareira, decoração sofisticada, com projeto diferenciado em Gladistone, mármore branco puríssimo e porcelanato, especialmente detalhado item por item, para total conforto da família, porcelanato nas áreas frias, cozinha americana, projeto personalizado de marcenaria em todos os ambientes, painel de pedra na varanda gourmet, condomínio muito bem administrado, com custo baixo mensal, localização privilegiada, com fácil acesso para Brás Leme.", "\n" +
             "Excelente apto semi mobiliado a uma quadra do metro Santana, e do parque da Juventude, próximo a comércios, bancos, mercados, lojas, escolas, shopping center norte, grandes avenidas; 02 dorm. sendo 01 suite com closet, sala, cozinha, 01 wc, lavanderia, 01 vaga livre e coberta, portaria 24 horas. Locação somente com Seguro Fiança ou Fiador.\n"};
